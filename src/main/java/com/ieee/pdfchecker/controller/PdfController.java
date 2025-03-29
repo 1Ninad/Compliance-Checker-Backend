ccc7bca4de57a5c96a2736b16e9bdab530388b94
Ninad Kale <ninadkale200@gmail.com>
2025-03-29 17:52:43 +0530

    cors - PdfController

--- src/main/java/com/ieee/pdfchecker/controller/PdfController.java
+++ src/main/java/com/ieee/pdfchecker/controller/PdfController.java
@@ -9,7 +9,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.File;
 import java.io.IOException;
 
-@CrossOrigin(origins = "http://localhost:3000") // Allow frontend access
+@CrossOrigin(origins = "https://compliance-checker-nk.vercel.app") // Allow frontend access
 @RestController
 @RequestMapping("/api/pdf")
 public class PdfController {
